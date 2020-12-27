7a6c8c77139cef0e864c59bd85c2b38b8513284f
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-27 22:04:01 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -27,17 +27,39 @@ void combine(Node* head, Node* head2) {
 	link->next=head2;
 }
 
+void sort(Node* head) {
+	int swapped;
+	Node* ptr1;
+	Node* lptr=NULL;
+	if(head==NULL)
+		return;
+	do {
+		swapped=0;
+		ptr1=head;
+		while(ptr1->next!=lptr) {
+			if(ptr1->data>ptr1->next->data) {
+				int tmp=ptr1->data;
+				ptr1->data=ptr1->next->data;
+				ptr1->next->data=tmp;
+				swapped=1;
+			}
+			ptr1=ptr1->next;
+		}
+		lptr=ptr1;
+	} while(swapped);
+}
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	
 	Node* head=NULL;
 	Node* tmp = new Node();
-	tmp->data=1;
+	tmp->data=10;
 	tmp->next=NULL;
 	Node* trav;
 	head=trav=tmp;
-	for(int i=2; i<=10; ++i) {
+	for(int i=11; i<=20; ++i) {
 		tmp = new Node();
 		tmp->data=i;
 		tmp->next=NULL;
@@ -47,10 +69,10 @@ int main() {
 	print(head);
 	Node* head2=NULL;
 	tmp = new Node();
-	tmp->data=10;
+	tmp->data=1;
 	tmp->next=NULL;
 	head2=trav=tmp;
-	for(int i=11; i<=20; ++i) {
+	for(int i=2; i<=10; ++i) {
 		tmp = new Node();
 		tmp->data=i;
 		tmp->next=NULL;
@@ -59,5 +81,7 @@ int main() {
 	}
 	print(head2);
 	combine(head, head2);
+	print(head);
+	sort(head);
 	print(head);
 }
