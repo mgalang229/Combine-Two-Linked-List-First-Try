5f5f27c55a9a24b71e2ea316d494f5834d430bb7
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-01-07 23:50:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
-using namespace std;
 
-#define ll long long
-#define ar array
+using namespace std;
 
 class Node {
 	public:
@@ -10,87 +8,88 @@ class Node {
 		Node* next;
 };
 
-void print(Node* head) {
-	Node* tmp=head;
-	while(tmp!=NULL) {
+void Print(Node* head) {
+	Node* tmp = head;
+	while (tmp != NULL) {
 		cout << tmp->data << " ";
-		tmp=tmp->next;
+		tmp = tmp->next;
 	}
-	cout << "\n";
+	cout << '\n';
 }
 
-void combine(Node* head, Node* head2) {
-	Node* con=head;
-	if(head==NULL||head2==NULL)
+void Combine(Node* head, Node* head2) {
+	Node* connect = head;
+	if (head == NULL || head2 == NULL) {
 		return;
-	while(con->next!=NULL)
-		con=con->next;
-	con->next=head2;
+	}
+	while (connect->next != NULL) {
+		connect = connect->next;
+	}
+	connect->next = head2;
 }
 
-void sort(Node* head) {
-	bool ok=0;
+void BubbleSort(Node* head) {
+	bool checker = false;
 	Node* ptr;
-	Node* lptr=NULL;
-	if(head==NULL)
+	Node* lptr = NULL;
+	if (head == NULL) {
 		return;
+	}
 	do {
-		ok=0;
-		ptr=head;
-		while(ptr->next!=lptr) {
-			if(ptr->data>ptr->next->data) {
-				int tmp=ptr->data;
-				ptr->data=ptr->next->data;
-				ptr->next->data=tmp;
-				ok=1;
+		checker = false;
+		ptr = head;
+		while (ptr->next != lptr) {
+			if (ptr->data > ptr->next->data) {
+				int tmp = ptr->data;
+				ptr->data = ptr->next->data;
+				ptr->next->data = tmp;
+				checker = true;
 			}
-			ptr=ptr->next;
+			ptr = ptr->next;
 		}
-		lptr=ptr;
-	} while(ok);
+		lptr = ptr;
+	} while(checker);
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	//create first linked list from 10->20
-	Node* head=NULL;
+	// create 1st linked list from 10 to 20
+	Node* head = NULL;
 	Node* tmp = new Node();
-	tmp->data=10;
-	tmp->next=NULL;
+	tmp->data = 10;
+	tmp->next = NULL;
 	Node* trav;
-	head=trav=tmp;
-	for(int i=11; i<=20; ++i) {
+	head = trav = tmp;
+	for (int i = 11; i <= 20; i++) {
 		tmp = new Node();
-		tmp->data=i;
-		tmp->next=NULL;
-		trav->next=tmp;
-		trav=trav->next;
+		tmp->data = i;
+		tmp->next = NULL;
+		trav->next = tmp;
+		trav = trav->next;
 	}
-	//print first linked list
-	print(head);
-	//create second linked list 1->10
-	Node* head2=NULL;
+	// print 1st linked list
+	Print(head);
+	// create 2nd linked list from 1 to 10
+	Node* head2 = NULL;
 	tmp = new Node();
-	tmp->data=1;
-	tmp->next=NULL;
-	head2=trav=tmp;
-	for(int i=2; i<=10; ++i) {
+	tmp->data = 1;
+	tmp->next = NULL;
+	head2 = trav = tmp;
+	for (int i = 2; i <= 10; i++) {
 		tmp = new Node();
-		tmp->data=i;
-		tmp->next=NULL;
-		trav->next=tmp;
-		trav=trav->next;
+		tmp->data = i;
+		tmp->next = NULL;
+		trav->next = tmp;
+		trav = trav->next;
 	}
-	//print second linked list
-	print(head2);
-	//combine first and second linked list
-	combine(head, head2);
-	// print the newly-created linked list
-	print(head);
-	//sort the newly-created linked list using bubble-sort
-	sort(head);
-	//print the sorted linked list
-	print(head); 
+	// print 2nd linked list
+	Print(head2);
+	// combine 1st and 2nd linked list
+	Combine(head, head2);
+	// print newly-combined linked list
+	Print(head);
+	// sort the newly-combined linked list using bubble-sort
+	BubbleSort(head);
+	// print the sorted linked list
+	Print(head);
+	return 0;
 }
